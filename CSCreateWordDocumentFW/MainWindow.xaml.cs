d286427ef53f4b76b91728ca8eb627114a2da49f
d7p4n4 <bercikezs199999999@gmail.com>
2020-04-08 09:40:55 +0200

    Add project files.

---
+++ CSCreateWordDocumentFW/MainWindow.xaml.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using Word = Microsoft.Office.Interop.Word;
+using System.Reflection;
+using Microsoft.Office.Interop.Word;
+using System.IO;
+using System.Windows.Forms;
+
+namespace CSCreateWordDocumentFW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateDocument();
+        }
+
+        //Create document method  
+        private void CreateDocument()
+        {
+            try
+            {
+                //Create an instance for word app  
+                Microsoft.Office.Interop.Word.Application winword = new Microsoft.Office.Interop.Word.Application();
+
+
+                //Set status for word application is to be visible or not.  
+                winword.Visible = false;
+
+                //Create a missing variable for missing value  
+                object missing = System.Reflection.Missing.Value;
+
+                //Create a new document  
+                Microsoft.Office.Interop.Word.Document document = winword.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+
+                //Add header into the document  
+                foreach (Microsoft.Office.Interop.Word.Section section in document.Sections)
+                {
+                    //Get the header range and add the header details.  
+                    Microsoft.Office.Interop.Word.Range headerRange = section.Headers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    headerRange.Fields.Add(headerRange, Microsoft.Office.Interop.Word.WdFieldType.wdFieldPage);
+                    headerRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    headerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdBlue;
+                    headerRange.Font.Size = 10;
+                    headerRange.Text = "Header text goes here";
+                }
+
+                //Add the footers into the document  
+                foreach (Microsoft.Office.Interop.Word.Section wordSection in document.Sections)
+                {
+                    //Get the footer range and add the footer details.  
+                    Microsoft.Office.Interop.Word.Range footerRange = wordSection.Footers[Microsoft.Office.Interop.Word.WdHeaderFooterIndex.wdHeaderFooterPrimary].Range;
+                    footerRange.Font.ColorIndex = Microsoft.Office.Interop.Word.WdColorIndex.wdDarkRed;
+                    footerRange.Font.Size = 10;
+                    footerRange.ParagraphFormat.Alignment = Microsoft.Office.Interop.Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    footerRange.Text = "Footer text goes here";
+                }
+
+                //adding text to document  
+                document.Content.SetRange(0, 0);
+                document.Content.Text = "This is test document " + Environment.NewLine;
+
+                //Add paragraph with Heading 1 style  
+                Microsoft.Office.Interop.Word.Paragraph para1 = document.Content.Paragraphs.Add(ref missing);
+                object styleHeading1 = "Heading 1";
+                para1.Range.set_Style(ref styleHeading1);
+                para1.Range.Text = "Para 1 text";
+                para1.Range.InsertParagraphAfter();
+
+                //Add paragraph with Heading 2 style  
+                Microsoft.Office.Interop.Word.Paragraph para2 = document.Content.Paragraphs.Add(ref missing);
+                object styleHeading2 = "Heading 2";
+                para2.Range.set_Style(ref styleHeading2);
+                para2.Range.Text = "Neve: #NEV#";
+                para2.Range.InsertParagraphAfter();
+
+                //Create a 5X5 table and insert some dummy record  
+                Word.Table firstTable = document.Tables.Add(para1.Range, 3, 2, ref missing, ref missing);
+
+                firstTable.Borders.Enable = 1;
+                foreach (Row row in firstTable.Rows)
+                {
+                    foreach (Cell cell in row.Cells)
+                    {
+                        //Header row  
+                        if (cell.RowIndex == 1)
+                        {
+                            cell.Range.Text = "Column " + cell.ColumnIndex.ToString();
+                            cell.Range.Font.Bold = 1;
+                            //other format properties goes here  
+                            cell.Range.Font.Name = "verdana";
+                            cell.Range.Font.Size = 10;
+                            //cell.Range.Font.ColorIndex = WdColorIndex.wdGray25;                              
+                            cell.Shading.BackgroundPatternColor = WdColor.wdColorGray25;
+                            //Center alignment for the Header cells  
+                            cell.VerticalAlignment = WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                            cell.Range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+
+                        }
+                        //Data row  
+                        else
+                        {
+                            cell.Range.Text = "Tartalom " + (cell.RowIndex - 2 + cell.ColumnIndex).ToString();
+                        }
+                    }
+                }
+
+                //Save the document  
+                object filename = @"d:\temp1.docx";
+                document.SaveAs(ref filename);
+                document.Close(ref missing, ref missing, ref missing);
+                document = null;
+                winword.Quit(ref missing, ref missing, ref missing);
+                winword = null;
+                System.Windows.Forms.MessageBox.Show("Document created successfully !");
+            }
+            catch (Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void buttonChange_Click(object sender, EventArgs e)
+        {
+            //  create offer letter
+            try
+            {
+                File.Copy("d:\\temp1.docx", "d:\\temp.docx", true);
+                //  create missing object
+                object missing = Missing.Value;
+                //  create Word application object
+                Word.Application wordApp = new Word.Application();
+                //  create Word document object
+                Word.Document aDoc = null;
+                //  create & define filename object with temp.doc
+                object filename = "d:\\temp.docx";
+                //  if temp.doc available
+                if (File.Exists((string)filename))
+                {
+                    object readOnly = false;
+                    object isVisible = false;
+                    //  make visible Word application
+                    wordApp.Visible = false;
+                    //  open Word document named temp.doc
+                    aDoc = wordApp.Documents.Open(ref filename, ref missing,
+        ref readOnly, ref missing, ref missing, ref missing,
+        ref missing, ref missing, ref missing, ref missing,
+        ref missing, ref isVisible, ref missing, ref missing,
+        ref missing, ref missing);
+                    aDoc.Activate();
+                    //  Call FindAndReplace()function for each change
+                    this.FindAndReplace(wordApp, "#NEV#", "Lucifer");
+                    //  save temp.doc after modified
+                    aDoc.Save();
+                }
+                else
+                    System.Windows.Forms.MessageBox.Show("File does not exist.",
+            "No File", MessageBoxButtons.OK,
+            MessageBoxIcon.Information);
+            }
+            catch (Exception)
+            {
+                System.Windows.Forms.MessageBox.Show("Error in process.", "Internal Error",
+        MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void FindAndReplace(Word.Application wordApp,
+            object findText, object replaceText)
+        {
+            object matchCase = true;
+            object matchWholeWord = true;
+            object matchWildCards = false;
+            object matchSoundsLike = false;
+            object matchAllWordForms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiacritics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            wordApp.Selection.Find.Execute(ref findText, ref matchCase,
+                ref matchWholeWord, ref matchWildCards, ref matchSoundsLike,
+                ref matchAllWordForms, ref forward, ref wrap, ref format,
+                ref replaceText, ref replace, ref matchKashida,
+                        ref matchDiacritics,
+                ref matchAlefHamza, ref matchControl);
+        }
+
+    }
+
+}
